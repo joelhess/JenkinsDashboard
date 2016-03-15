c6b618fbe2b947056bb757da91bb90844a3724fc
joelhess <hess.joel@gmail.com>
2016-03-14 19:52:22 -0500

    Initial Commit

---
+++ JenkinsClient/BuildProject.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsClient
+{
+    //internal class BuildProject 
+    //{
+    //    public string displayName { get; set; }
+    //    public string description { get; set; }
+    //    public Uri url { get; set; }
+    //    public List<Build> BuildList { get; set; }
+
+    //    public string LastBuildStatus
+    //    {
+    //        get
+    //        {
+    //            if (BuildList != null && BuildList.Count > 0)
+    //                return BuildList[0].status;
+    //            else
+    //                return "UNKNOWN";
+    //        }
+    //    }
+    //}
+
+    public class BuildProject
+    {
+        public List<Action> actions { get; set; }
+        public string description { get; set; }
+        public string displayName { get; set; }
+        public object displayNameOrNull { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public bool buildable { get; set; }
+        public List<Build> builds { get; set; }
+        public string color { get; set; }
+        public Build firstBuild { get; set; }
+        public List<HealthReport> healthReport { get; set; }
+        public bool inQueue { get; set; }
+        public bool keepDependencies { get; set; }
+        public Build lastBuild { get; set; }
+        public Build lastCompletedBuild { get; set; }
+        public Build lastFailedBuild { get; set; }
+        public Build lastStableBuild { get; set; }
+        public Build lastSuccessfulBuild { get; set; }
+        public Build lastUnstableBuild { get; set; }
+        public Build lastUnsuccessfulBuild { get; set; }
+        public int nextBuildNumber { get; set; }
+        public List<Property> property { get; set; }
+        public object queueItem { get; set; }
+        public bool concurrentBuild { get; set; }
+        public List<object> downstreamProjects { get; set; }
+        public Scm scm { get; set; }
+        public List<object> upstreamProjects { get; set; }
+    }
+
+    public class Build
+    {
+        public int Number { get; set; }
+        public string url { get; set; }
+    }
+
+    public class HealthReport
+    {
+        public string description { get; set; }
+        public string iconClassName { get; set; }
+        public string iconUrl { get; set; }
+        public int score { get; set; }
+    }
+
+    public class Scm
+    {
+    }
+
+    public class Action
+    {
+    }
+
+    public class Property
+    {
+    }
+}
---
+++ JenkinsClient/JenkinsDataLoader.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsClient
+{
+    public class JenkinsDataLoader
+    {
+        string JenkinsServerUrl = "https://ci.jenkins-ci.org/job/jenkins_pom/api/json";
+
+        public async void GetProjectData(string url)
+        {
+
+            var requestUri = new Uri(JenkinsServerUrl);
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                var resp = await client.GetAsync(requestUri);
+
+                string response = await resp.Content.ReadAsStringAsync();
+                var jObject = JObject.Parse(response);
+                var jsonResponse = JsonConvert.DeserializeObject<BuildProject>(response);
+
+                //var name = jObject["displayName"];
+            }
+
+        }
+
+        public async Task<Build> GetBuildStatus(string Url)
+        {
+            var requestUri = new Uri(Url);
+
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                var resp = await client.GetAsync(requestUri);
+
+                string response = await resp.Content.ReadAsStringAsync();
+                var jsonResponse = JsonConvert.DeserializeObject(response);
+                var build = new Build();
+
+                return build;
+            }
+
+
+        }
+    }
+}
